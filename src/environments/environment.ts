ee28c1a88befc7f63fc8722d13686ca52e435095
Ekta Singh <ekta.myself29jan@gmail.com>
2023-01-11 09:48:16 +0530

    fronten package update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   secretKey: '6LfFILUgAAAAAFYUPCUPOHNB4xkGe_gHnoirGvE_',
-  baseUrl:'https://zany-plum-millipede-gown.cyclic.app'
+  baseUrl:'http://localhost:300'
 };
