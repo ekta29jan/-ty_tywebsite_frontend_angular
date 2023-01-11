9f9f522ef04fa7438fe97b81ab4e060073e7853f
Ekta Singh <ekta.myself29jan@gmail.com>
2023-01-11 11:48:04 +0530

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   secretKey: '6LfFILUgAAAAAFYUPCUPOHNB4xkGe_gHnoirGvE_',
-  baseUrl:'https://technoelevate-website.cyclic.app'
+  baseUrl:'https://technoelevate-website.cyclic.app:4200'
 };
